f5a04b855a5a1b8699a7aca5d5b007cc1ebd389c
Isabela Espinoza <isabela.espinoza@correo.unimet.edu.ve>
2023-03-07 19:48:32 -0400

    Footer
    Footer por pulir

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import Footer from "./Footer";
 import Home from './views/home/home'
 import Loginoption from './views/loginoption/loginoption'
 
@@ -12,8 +13,15 @@ function App() {
         <Route path='/' element={<Home/>} />
         <Route path='h' element={<Loginoption/>} />
       </Routes>
+      <div className="App">
+        <Footer />
+      </div>
     </div>
+    
   )          
 }
 
 export default App
+
+
+//export default App
