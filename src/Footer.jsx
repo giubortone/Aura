23833594ba1dec1b2f16b9953d734b411967907b
Giulianna Bortone <70867985+Giuliannabortone@users.noreply.github.com>
2023-03-09 14:42:52 -0400

    commit home le falta reviews con el .map

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 //const Footer = () => {
 //    const year = new Date().getFullYear();
 //  
-//    return <footer className='text-violet-900'>
+//    return <footer className='text-purple-900'>
 //    <div className='md:flex md:justify-between md:items-center sm:px-12 px-4 py-7'>
 //        <h1 className="lg:text-4x1 text-3x1 md:mb-0 mb-6 lg:leading-normal font-semibol md:w-2/5">holaaaaa</h1>
 //        <h2>Soy Footer</h2>
@@ -15,14 +15,14 @@ const Footer = () => {
     const year = new Date().getFullYear();
   
     return <footer
-    class="text-center text-neutral-600 dark:text-violet-900 lg:text-left">
+    class="text-center text-neutral-600 dark:text-purple-900 lg:text-left">
     <div
       class="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-neutral-500 lg:justify-between">
       <div class="mr-12 hidden lg:block">
         <span>Contactanos en nuestras redes sociales:</span>
       </div>
       <div class="flex justify-center">
-        <a href="#!" class="mr-6 text-neutral-600 dark:text-violet-900">
+        <a href="#!" class="mr-6 text-neutral-600 dark:text-purple-900">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-5 w-5"
@@ -34,7 +34,7 @@ const Footer = () => {
               clip-rule="evenodd" />
           </svg>
         </a>
-        <a href="#!" class="mr-6 text-neutral-600 dark:text-violet-900">
+        <a href="#!" class="mr-6 text-neutral-600 dark:text-purple-900">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-4 w-4"
@@ -44,7 +44,7 @@ const Footer = () => {
               d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
           </svg>
         </a>
-        <a href="#!" class="text-neutral-600 dark:text-violet-900">
+        <a href="#!" class="text-neutral-600 dark:text-purple-900">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-4 w-4"
@@ -71,12 +71,12 @@ const Footer = () => {
             Nosotros
           </h6>
           <p class="mb-4">
-            <a href="#!" class="text-neutral-600 dark:text-violet-900"
+            <a href="#!" class="text-neutral-600 dark:text-purple-900"
               >Nosotros</a
             >
           </p>
           <p class="mb-4">
-            <a href="#!" class="text-neutral-600 dark:text-violet-900"
+            <a href="#!" class="text-neutral-600 dark:text-purple-900"
               >Preguntas frecuentes</a
             >
           </p>
@@ -144,7 +144,7 @@ const Footer = () => {
     <div class=" p-6 text-center">
       <span>© 2023 Copyright:</span>
       <a
-        class="font-semibold text-violet-900 dark:text-violet-900"
+        class="font-semibold text-purple-900 dark:text-purple-900"
         href="https://tailwind-elements.com/"
         >Aura</a
       >
--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
- export default function Home() {
+//  export default function Home() {
+//     let testiomonios = {
+//         img: 'src/assets/Ellipse 13.png',
+//         name: 'Jose Luis Guerrero',
+//     }
+
+
+//     let cuadradosTestimonios= testiomonios.map((element, i) =>)
+
+
+
 
 
     return(
         <React.Fragment>
             <div className="flex justify-between items-center bg-gray-100">
                 <div className='flex flex-col items-center ml-24'>
-                   <h1 className='text-3xl mb-4 font-bold'>Descubre psicologos en linea</h1> 
+                   <h1 className='text-3xl mb-4 font-bold'>Descubre  psicólogos en línea</h1> 
                    <h2 className='text-xl'>Tu salud mental es lo más importante, una <br/> mente sana hace un cuerpo sano</h2> <br/>
                     <div className="">
-                        <button  className='bg-violet-900 rounded-lg p-2 px-4  text-white mr-4'>Saber mas</button>
+                        <button  className='bg-violet-900 rounded-lg p-2 px-4  text-white mr-4'>Saber más</button>
 
                         <button   className='bg-violet-900 rounded-lg p-2 px-4 text-white ml-4'>Pedir cita</button>
                     
@@ -24,15 +34,15 @@ import React from 'react'
                 <div>
                     <div className='flex justify-center items-center'> 
                         <img className='' src='src\assets\Counter.png' alt='img2' />
-                        <h2 className='font-bold text-2xl ml-6'>Registrate <br/> Gratis</h2>
+                        <h2 className='font-bold text-2xl ml-6 text-violet-900'>Registrate <br/> Gratis</h2>
                     </div>
                     <div className='mt-3 flex text-center'> Registrate y revisa nuestra amplia lista de <br /> doctores, elige el que te inspire mas <br /> confianza </div>
                 </div>
 
                 <div >
                     <div className='flex justify-center items-center'> 
-                        <img className='' src='src\assets\Counter.png' alt='img2' />
-                        <h2 className='font-bold text-2xl ml-6'>Pide tu cita <br/> Gratis</h2>
+                      \\\\
+                        <h2 className='font-bold text-2xl ml-6 text-violet-900'>Pide tu cita <br/> Gratis</h2>
                     </div>
                     <div className='mt-3 flex text-center'> Registrate y revisa nuestra amplia lista de <br /> doctores, elige el que te inspire mas <br /> confianza </div>
                 </div>
@@ -40,7 +50,7 @@ import React from 'react'
                 <div>
                     <div className='flex justify-center items-center'> 
                         <img className='' src='src\assets\Counter.png' alt='img2' />
-                        <h2 className='font-bold text-2xl ml-6'> Pide tu cira <br/> Gratis</h2>
+                        <h2 className='font-bold text-2xl ml-6 '> Pide tu cita <br/> Gratis</h2>
                     </div>
                     <div className='mt-3 flex text-center'> Registrate y revisa nuestra amplia lista de <br /> doctores, elige el que te inspire mas <br /> confianza </div>
                 </div>
@@ -49,9 +59,10 @@ import React from 'react'
             <div className='mt-24'>
                 <img className="absolute z-0 opacity-80" src="src\assets\SOMOS SIGNATARIOS DE LA O,,,NU 1.png" alt="img-3" />
                 <div className=' relative z-5 flex justify-end grid mr-24' >
-                    <h1 className='font-bold text-5xl mt-24' >  Eres psicologo? <br /></h1>  
+                    <h1 className='font-bold text-5xl mt-24' > ¿Eres psicólogo? <br /></h1>  
                     <p className='mt-7 text-xl mb-8'>Tendrás un espacio en el cual personas que <br /> requieren de tus servicios podrán agendar citas <br /> contigo según tu agenda de disponibilidad. </p>
                     <p className='mb-10'> Con Aura tendrá acceco a:</p>
+                    
 
                     <div className='flex justify-around space-x-2'>
                         <h4 className='border border-black w-1/3 text-center rounded-lg bg-violet-900 text-white'> Feedback</h4>
@@ -66,7 +77,7 @@ import React from 'react'
                 <div className='flex justify-start grid ml-24'>
                     <h1 className='font-bold text-4xl' > Conoce a nuestros profesionales <br /></h1>  
                     <p className='mt-7 text-xl mb-8'>Tendrás un espacio en el cual personas que <br /> requieren de tus servicios podrán agendar citas <br /> contigo según tu agenda de disponibilidad. </p>
-                    <p className='mb-10'> Con Aura puedes buscar a los mejores psicologos:</p>
+                    <p className='mb-10 font-bold text-3xl '> Con Aura puedes buscar a los mejores psicologos</p>
                 </div>
                 
                 <img className='mt-0' src="src\assets\Grey Untangled Thoughts Mental Health Clinic Logo 1.png" alt="img4" />
@@ -113,12 +124,12 @@ import React from 'react'
                 <h1 className='px-11 py-4'>Lo que opinan nuestros pacientes</h1>
             </div>
 
-            <div className='flex justify-evenly items-center'>
-                <div className='border-2 rounded-lg w-96 border-black px-2 y'>
-                    <div className='flex justify-center'>
+            <div className='flex justify-evenly items-center mt-8'>
+                <div className='border-2 rounded-lg w-96 border-black px-2 '>
+                    <div className='flex justify-center mt-6'>
                         <div className='flex justify-center'>
                              <img src="src\assets\Ellipse 13.png" alt="img7" />
-                             <div className='px-8'>
+                             <div className='px-8' >
                                 <h1>Jose Luis Guerrero</h1>
                                 <p>12/4/17</p>
                             </div>
@@ -132,7 +143,7 @@ import React from 'react'
                         </div>
                     </div>
 
-                    <p className='mt-4'>Mi Dr me ha enseñado otra perspectiva de los <br /> psicólogos, tengo 73 años y siempre pensé que era <br /> para locos jeje.</p>
+                    <p className='mt-4 mb-4'>Mi Dr me ha enseñado otra perspectiva de los <br /> psicólogos, tengo 73 años y siempre pensé que era <br /> para locos jeje.</p>
 
                     
 
@@ -148,7 +159,12 @@ import React from 'react'
                     <h1> Hola</h1>
                 </div>
             </div>
+
+
+
+
+
+
         
         </React.Fragment>
     )
-}
