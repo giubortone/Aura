7d7ad0cdb85d9bfd73eaf5d5f681db9f3a63c8ba
Samm07 <samuelleonardomoreno@gmail.com>
2023-03-21 15:51:53 -0400

    Nav Bar

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,12 @@ import './index.css'
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/RouterProvider";
 import Footer from './Footer'
+import NavBarr from './components/NavBar'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <NavBarr/>
     <RouterProvider router={router} />
     <Footer />
   </React.StrictMode>,
